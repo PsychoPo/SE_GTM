94125707576ccc5375615804da86107af76fa7b8
Roman <psychopo@icloud.com>
2024-03-15 17:52:45 +0700

    fixed&&added res

---
+++ res/res_rc.py
@@ -0,0 +1,119 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.6.2
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01\xc6\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4\x22 viewBox=\x220 -9\
+60 960 960\x22 widt\
+h=\x2224\x22><path d=\x22\
+m612-292 56-56-1\
+48-148v-184h-80v\
+216l172 172ZM480\
+-80q-83 0-156-31\
+.5T197-197q-54-5\
+4-85.5-127T80-48\
+0q0-83 31.5-156T\
+197-763q54-54 12\
+7-85.5T480-880q8\
+3 0 156 31.5T763\
+-763q54 54 85.5 \
+127T880-480q0 83\
+-31.5 156T763-19\
+7q-54 54-127 85.\
+5T480-80Zm0-400Z\
+m0 320q133 0 226\
+.5-93.5T800-480q\
+0-133-93.5-226.5\
+T480-800q-133 0-\
+226.5 93.5T160-4\
+80q0 133 93.5 22\
+6.5T480-160Z\x22/><\
+/svg>\
+\x00\x00\x02\xb0\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 height=\x222\
+4\x22 viewBox=\x220 -9\
+60 960 960\x22 widt\
+h=\x2224\x22><path d=\x22\
+M444-200h70v-50q\
+50-9 86-39t36-89\
+q0-42-24-77t-96-\
+61q-60-20-83-35t\
+-23-41q0-26 18.5\
+-41t53.5-15q32 0\
+ 50 15.5t26 38.5\
+l64-26q-11-35-40\
+.5-61T516-710v-5\
+0h-70v50q-50 11-\
+78 44t-28 74q0 4\
+7 27.5 76t86.5 5\
+0q63 23 87.5 41t\
+24.5 47q0 33-23.\
+5 48.5T486-314q-\
+33 0-58.5-20.5T3\
+90-396l-66 26q14\
+ 48 43.5 77.5T44\
+4-252v52Zm36 120\
+q-83 0-156-31.5T\
+197-197q-54-54-8\
+5.5-127T80-480q0\
+-83 31.5-156T197\
+-763q54-54 127-8\
+5.5T480-880q83 0\
+ 156 31.5T763-76\
+3q54 54 85.5 127\
+T880-480q0 83-31\
+.5 156T763-197q-\
+54 54-127 85.5T4\
+80-80Zm0-80q134 \
+0 227-93t93-227q\
+0-134-93-227t-22\
+7-93q-134 0-227 \
+93t-93 227q0 134\
+ 93 227t227 93Zm\
+0-320Z\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x08\
+\x008U\xa7\
+\x00t\
+\x00i\x00m\x00e\x00.\x00s\x00v\x00g\
+\x00\x09\
+\x00V\x8e\x07\
+\x00c\
+\x00o\x00i\x00n\x00s\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x8e'\x8d\x1a\xbb\
+\x00\x00\x00&\x00\x00\x00\x00\x00\x01\x00\x00\x01\xca\
+\x00\x00\x01\x8e'\x8cr\xda\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
